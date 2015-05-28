0abb5a62896525e5632468e503e8af89f3b4f6a8
SudoQ <simj91@gmail.com>
2015-05-28 22:37:06 +0200

    Added neccecary functions

--- blend.js
+++ blend.js
@@ -1,7 +1,49 @@
-var color1 = "FF";
-var color2 = "F0";
+String.prototype.repeat = function( num )
+{
+    return new Array( num + 1 ).join( this );
+}
 
-console.log(parseInt(color1,16).toString(2));
-console.log(parseInt(color2,16).toString(2));
-var tmp = color1 & color2;
-console.log(tmp.toString(2));
+function and(x,y){
+	return x & y;
+}
+function or(x,y){
+	return x | y;
+}
+function xor(x,y){
+	return x ^ y;
+}
+function blend(hex1, hex2, op){
+	return op(parseInt(hex1, 16), parseInt(hex2, 16))
+}
+function blendInput(){
+	var c1 = document.getElementById("myCanvas")
+}
+/*
+var color1 = "F0F";
+var color2 = "FF0";
+
+funcs = [and, or, xor];
+funcs.forEach(function(entry){
+	console.log(blend(color1, color2, entry).toString(16));
+});
+*/
+
+String.prototype.repeat = function( num )
+{
+    return new Array( num + 1 ).join( this );
+}
+function hexStrToInt(hexStr){
+	var i = 1;
+	if(hexStr.indexOf("#") == -1){
+		i = 0;
+	}
+	return parseInt(hexStr.substring(i),16);
+}
+
+function intToHexStr(i){
+	var s = i.toString(16);
+	return "#"+"0".repeat(6-s.length)+s;
+}
+
+console.log(hexStrToInt("#F0F0F0"));
+console.log(intToHexStr(15790320));
